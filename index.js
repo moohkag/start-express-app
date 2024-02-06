4b0592ac7264e608ad3639e5f65e25c6205840f7
moohka <moohka@hotmail.com>
2024-02-06 04:13:33 -0600

    mongodb connection

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /* requires */
 const express = require("express");
 const mongoose = require("mongoose");
-// const createRoute = require("./src/routes/createRoute");
+const createRoute = require("./src/routes/createRoute");
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send({ message: "welcome to Pixely API." });
 });
-// app.use("/create", createRoute);
+app.use("/create", createRoute);
 
 /* Connections */
-// mongoose.connect("d").catch((error) => {
-//   console.log("MongoDB not connected");
-// });
+mongoose.connect(process.env.MONGODB_URI).catch((error) => {
+  console.log("MongoDB not connected", error);
+});
 app.listen(process.env.PORT, (error) => {
-  console.log("listening to port 4000");
+  console.log("listening to port 4000, PORT:", process.env.PORT);
 });
