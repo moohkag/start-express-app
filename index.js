d0b91a4f104448177468f527867e9d843bc32f00
moohka <moohka@hotmail.com>
2024-02-25 10:58:48 -0600

    update

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 /* requires */
+if (process.env.DOTENV === undefined) {
+  require("dotenv").config();
+}
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
