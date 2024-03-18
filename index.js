6cee02beb24227f7be7cc2c37b9aa463afe53e89
moohka <moohka@hotmail.com>
2024-03-18 18:48:49 -0500

    fix

--- index.js
+++ index.js
@@ -18,13 +18,14 @@ const app = express();
 
 // CORS setting
 const CORSOptions = {
-  origin: [
-    "https://pixely.ca",
-    /https:\/\/.*\.pixely.ca$/,
+  // origin: [
+  //   "https://pixely.ca",
+  //   /https:\/\/.*\.pixely.ca$/,
 
-    "http://localhost:3000",
-    /http:\/\/.*\.localhost:3000$/,
-  ],
+  //   "http://localhost:3000",
+  //   /http:\/\/.*\.localhost:3000$/,
+  // ],
+  origin: "https://pixely.ca",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
